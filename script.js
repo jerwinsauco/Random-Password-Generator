42aaad14a6522d36df933299b1793dbf3cd38194
Jerwin <jerwinsauco@gmail.com>
2022-07-19 22:19:07 -0400

    Ready for Grading

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ function generatePassword() {
     alert("No input entered! Goodbye");
   }
 
-  //Need to debug this. Should only accept numeric entry. No letters/words
   else if (isNaN(length)) {
     alert("Use Numeric Number ONLY!");
   }
@@ -38,13 +37,13 @@ function generatePassword() {
   else if (length < 8 || length > 128) {
     length = prompt("Password must be from 8 to 128 in length.")
   }
+
   else {
     lowerCase = confirm("Include lowercase letters? Press OK if Yes. Cancel if No.");
     upperCase = confirm("Include uppercase letters? Press OK if Yes. Cancel if No.");
     numberConfirm = confirm("Include numeric in your new password? Press OK if Yes. Cancel if No.");
     specialCharacter = confirm("Include special characters in your new password? Press OK if Yes. Cancel if No.");
 
-    //All Options
     if (lowerCase) {
       selection += letters;
     }
@@ -57,56 +56,15 @@ function generatePassword() {
     if(specialCharacter) {
       selection += specialChars;
     }
-    console.log(selection)
-  for (var i = 0; i < length; i++){
-    //pick a random index number from selection string
-    var charIndex = Math.floor(Math.random() * selection.length)
-    //add the random char acter to password string
-     password += selection.charAt(charIndex) 
-  };
+
+    for (var i = 0; i < length; i++){
+      //pick a random index number from selection string
+      var charIndex = Math.floor(Math.random() * selection.length)
+      //add the random character to password string
+      password += selection.charAt(charIndex) 
+    };
 
   };
-
-  // if (lowerCase && upperCase && number && specialCharacter) {
-  // selection = character.concat(number, letters, lettersUp);
-  //}
-
-  //Three Options
-  // else if (number && specialCharacter && upperCase) {
-  //   selection = character.concat(number, lettersUp);
-  // } else if (specialCharacter && upperCase && lowerCase) {
-  //   selection = character.concat(letters, lettersUp);
-  // } else if (upperCase && lowerCase && number) {
-  //   selection = lettersUp.concat(letters, number);
-  // } else if (lowerCase && number && specialCharacter) {
-  //   selection = letters.concat(number, character);
-  // } 
-
-  //Two Options
-  // else if (specialCharacter && number) {
-  //   selection = character.concat(number);
-  // } else if (number && upperCase) {
-  //   selection = number.concat(lettersUp);
-  // } else if (upperCase && lowerCase) {
-  //   selection = lettersUp.concat(letters);
-  // } else if (lowerCase && number) {
-  //   selection = letters.concat(number);
-  // } else if (lowerCase && specialCharacter) {
-  //   selection = letters.concat(character);
-  // } else if (specialCharacter && upperCase) {
-  //   selection = character.concat(lettersUp);
-  // }
-
-  // //One Option
-  // else if (specialCharacter) {
-  //   selection = character;
-  // } else if (number) {
-  //   selection = number;
-  // } else if (lowerCase) {
-  //   selection = letters;
-  // } else if (upperCase) {
-  //   selection = lettersUp;
-  // };
 
   return password;
 }
